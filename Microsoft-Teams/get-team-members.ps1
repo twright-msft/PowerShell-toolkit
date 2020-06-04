8967bb38d44f55050251ea85b8b48f381f97763c
Travis Wright <twright@microsoft.com>
2020-06-04 10:41:15 -0700

    templatize get team members

--- Microsoft-Teams/get-team-members.ps1
+++ Microsoft-Teams/get-team-members.ps1
@@ -1,4 +1,4 @@
 Install-Module MicrosoftTeams
 Import-Module MicrosoftTeams
 Connect-MicrosoftTeams
-Get-Team -DisplayName "Azure Arc Customer Meetup" | ForEach-Object {Get-TeamUser -GroupID $_.GroupID -Role guest} | ?{$_.User -like '*.*'} | %{$_.User.Split('#')[0].ToLower() -replace '(.*)_(.*)','$1@$2'}
+Get-Team -DisplayName "<Team display name here>" | ForEach-Object {Get-TeamUser -GroupID $_.GroupID -Role guest} | ?{$_.User -like '*.*'} | %{$_.User.Split('#')[0].ToLower() -replace '(.*)_(.*)','$1@$2'}
